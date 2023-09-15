500c8cbf398084bbaa0c6581a534adfc5e310e07
Roughy <mdawn700@gmail.com>
2023-09-16 01:05:07 +0500

    initial commit

---
+++ HandOfLesser/HandOfLesser.cpp
@@ -0,0 +1,12 @@
+﻿// HandOfLesser.cpp : Defines the entry point for the application.
+//
+
+#include "HandOfLesser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
