e23fc9750d73fa7eb2e81bf8dc4ac13c9ddb90d3
Nordskog <mdawn700@gmail.com>
2025-10-17 17:17:48 +0500

    color body joints according to tracked state

--- HandOfLesser/src/openxr/body_tracking.cpp
+++ HandOfLesser/src/openxr/body_tracking.cpp
@@ -2,7 +2,6 @@
 #include "src/core/ui/user_interface.h"
 #include "XrUtils.h"
 
-
 void HOL::OpenXR::BodyTracking::init(xr::UniqueDynamicInstance& instance,
 									 xr::UniqueDynamicSession& session)
 {
@@ -16,33 +15,29 @@ void HOL::OpenXR::BodyTracking::updateBody(xr::UniqueDynamicSpace& space, XrTime
 
 void HOL::OpenXR::BodyTracking::drawBody()
 {
-	auto colorGrey = IM_COL32(200, 155, 155, 150);
-	auto colorWhite = IM_COL32(255, 255, 255, 150);
+	auto colorTracked = IM_COL32(0, 255, 0, 150);	 // Green semi-transparent for tracked
+	auto colorNotTracked = IM_COL32(255, 0, 0, 150); // Red semi-transparent for not tracked
 
 	auto vis = HOL::UserInterface::Current->getVisualizer();
 
+	XrBodyJointLocationFB* jointLocations = this->mBodyTracker.getLastJointLocations();
 
-		XrBodyJointLocationFB* jointLocations = this->mBodyTracker.getLastJointLocations();
+	for (int j = 0; j < XR_BODY_JOINT_COUNT_FB; j++)
+	{
+		XrBodyJointLocationFB& joint = jointLocations[j];
 
-		for (int j = 0; j < XR_BODY_JOINT_COUNT_FB; j++)
-		{
-			XrBodyJointLocationFB& joint = jointLocations[j];
+		// Check if joint is tracked
+		bool isTracked = (joint.locationFlags & XR_SPACE_LOCATION_POSITION_TRACKED_BIT)
+						 == XR_SPACE_LOCATION_POSITION_TRACKED_BIT;
 
-			auto color = colorGrey;
-			if (j == XR_BODY_JOINT_LEFT_HAND_WRIST_TWIST_FB)
-			{
-				color = IM_COL32(255, 0, 0, 255);
-			}
-		
+		auto color = isTracked ? colorTracked : colorNotTracked;
 
-			// WIll replace this later anyway so nevermind wasteful conversion
-			vis->submitPoint(OpenXR::toEigenVector(joint.pose.position), color, 7);
-		}
+		// WIll replace this later anyway so nevermind wasteful conversion
+		vis->submitPoint(OpenXR::toEigenVector(joint.pose.position), color, 7);
+	}
 }
 
 OpenXRBody& HOL::OpenXR::BodyTracking::getBodyTracker()
 {
 	return mBodyTracker;
 }
-
-
